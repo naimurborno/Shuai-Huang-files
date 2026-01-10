0b93f6a46a1df554059ea45fd2075002dac71ef9
naimurborno <naimurborno@gmail.com>
2026-01-10 21:53:15 +0600

    second phase files

--- train.py
+++ train.py
@@ -10,3 +10,16 @@ from tqdm import tqdm
 from datetime import datetime
 import os 
 from data_loader import Dataset, DataLoader
+import train_config
+# Filter the warning.
+import warnings
+
+warnings.filterwarnings(
+    "ignore", ".*Trying to infer the `batch_size` from an ambiguous collection.*"
+)
+if __name__ == "__main__":
+    # Get the config file
+    config = train_config.config
+    config["n_epochs"] =1# config['n_lin_epoch'] + config['n_dec_epoch']
+    # Set Path for data
+    root = config["data_path"]
