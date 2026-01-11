ab27b71347f23e6c491ee9667955c7053eded947
naimurborno <naimurborno@gmail.com>
2026-01-11 14:58:59 +0600

    fixed again

--- APPLY_PCA.py
+++ APPLY_PCA.py
@@ -6,5 +6,6 @@ def apply_pca(features,n_components=512):
     features=features.view(B*T, F)
     pca_model=PCA(n_components=n_components)
     F_roi_2d=pca_model.fit_transform(features)
+    F_roi_2d=F_roi_2d.to(torch.float32)
     F_roi=F_roi_2d.view(B, T, -1)
     return F_roi
