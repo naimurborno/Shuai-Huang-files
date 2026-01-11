1330190ec1af1bd011e418d8f79bc38b90de706d
naimurborno <naimurborno@gmail.com>
2026-01-11 15:22:28 +0600

    loader shape

--- train.py
+++ train.py
@@ -25,34 +25,34 @@ if __name__ == "__main__":
     # Get the config file
     config = train_config.config
     train_loader, val_loader, test_loader = create_dataloaders(
-    label_data_dir=config['label_data_dir'],
-    feature_data_dir=config['feature_data_dir'],
-    cluster_data_dir=config['cluster_data_dir'],
-    batch_size=8,
-    load_cluster=False,       # ← Set True only if you really need it
-)
-
-    # Get one batch to verify
-    batch = next(iter(train_loader))
-    print(batch.keys())
-    print("\nBatch shapes:")
-    print(f"features    : {batch['features'].shape}")     # [batch, 400, 1632]
-    print(f"labels      : {batch['label'].shape}") 
-    print(f"cluster     : {batch['cluster_map'].shape}")       # [batch]
-    print(f"subject_ids : {batch['subject_id']}")
-    print(f"feature dtype: {batch['features'].dtype}")
-    print(f"cluster dtype: {batch['cluster_map'].dtype}")
-
-    if 'cluster_map' in batch:
-        print(f"cluster_map : {batch['cluster_map'].shape}")
-
-    F=apply_pca(batch['features'])    
-    model = AtlasFreeBrainTransformer()
+                                            label_data_dir=config['label_data_dir'],
+                                            feature_data_dir=config['feature_data_dir'],
+                                            cluster_data_dir=config['cluster_data_dir'],
+                                            batch_size=config['batch_size']
+                                        )
+    print(train_loader.shape)
     
-    # F = torch.randn(2, 400, 1632)
-    # C = torch.randint(0, 401, (2, 45, 54, 45))
-    batch['cluster_map']=batch['cluster_map'].to(torch.long)
-    logits = model(F, batch['cluster_map'])
-    print(logits)  # (2, 2)
+    # Get one batch to verify
+    # batch = next(iter(train_loader))
+    # print(batch.keys())
+    # print("\nBatch shapes:")
+    # print(f"features    : {batch['features'].shape}")     # [batch, 400, 1632]
+    # print(f"labels      : {batch['label'].shape}") 
+    # print(f"cluster     : {batch['cluster_map'].shape}")       # [batch]
+    # print(f"subject_ids : {batch['subject_id']}")
+    # print(f"feature dtype: {batch['features'].dtype}")
+    # print(f"cluster dtype: {batch['cluster_map'].dtype}")
+
+    # if 'cluster_map' in batch:
+    #     print(f"cluster_map : {batch['cluster_map'].shape}")
+
+    # F=apply_pca(batch['features'])    
+    # model = AtlasFreeBrainTransformer()
+    
+    # # F = torch.randn(2, 400, 1632)
+    # # C = torch.randint(0, 401, (2, 45, 54, 45))
+    # batch['cluster_map']=batch['cluster_map'].to(torch.long)
+    # logits = model(F, batch['cluster_map'])
+    # print(logits)  # (2, 2)
     
 
