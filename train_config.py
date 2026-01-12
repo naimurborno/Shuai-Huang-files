170b8116a11038fc254a41e50f18dcedca18d2ef
naimurborno <naimurborno@gmail.com>
2026-01-12 11:25:48 +0600

    updated

--- train_config.py
+++ train_config.py
@@ -12,4 +12,4 @@ config['cluster_data_dir']="/content/Dataset_shuai_zip/Cluster_index_mat/"
 config['batch_size']=8
 config['Epochs']=10
 config['n_components']=812
-config['apply_pca']=False
+config['use_pca']=False
